4807aa895f57c4c00c4dc321312118d849236523
Magda Chikhradze <magda292005@gmail.com>
2026-03-25 15:23:02 +0400

    logical

---
+++ main.js
@@ -0,0 +1,45 @@
+let isItRaining = true;
+
+if (isItRaining === true) {
+  console.log("dont go outside");
+} else {
+  console.log("go outside");
+}
+
+let age = 35;
+
+if (age >= 65) {
+  console.log("you get pension");
+} else {
+  console.log("you dont get pension");
+}
+
+//
+let myAge = 25;
+
+if (myAge > 18 && myAge < 30) {
+  console.log("Valid age");
+}
+
+// && - logical 'and'
+// || - logical 'or'
+// ! - logical 'not'
+
+console.log(1 == true);
+
+// console.log(boolean(NaN));
+//NaN - not a number
+
+console.log(Number("9999"));
+console.log(Number("helllooo"));
+
+//
+
+let student = true;
+let age2 = 22;
+
+if (student && age2 > 20) {
+  console.log("you get a discount");
+} else {
+  console.log("you dont get a discount");
+}
