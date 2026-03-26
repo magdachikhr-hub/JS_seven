c4b2fd84c0d31a3127e668707131dd7d0d14f0a2
magda chikhradz <magda292005@gmail.com>
2026-03-26 15:00:45 +0400

    min and max

--- main-8.js
+++ main-8.js
@@ -39,3 +39,20 @@ let pages2 = Math.ceil(products / 10);
 console.log(pages2);
 
 console.log(Math.floor(Math.random() * 10));
+
+// daxarisxeba
+console.log(3 ** 2);
+console.log(Math.pow(7, 2));
+
+//kvadratuli pesvi
+console.log(Math.sqrt(25));
+
+console.log(27 ** (1 / 3));
+console.log(Math.pow(27, 1 / 3));
+
+//  min and max
+
+console.log(Math.min(12, 4, 6, 7, 8, 6, 9.272, 6));
+console.log(Math.max(12, 4, 6, 7, 8, 6, 9.272, 6));
+
+
