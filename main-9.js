0bafc788ba5e26ec1adcb7259a58a9db63f37fa1
magda chikhradz <magda292005@gmail.com>
2026-03-27 15:41:26 +0400

    function

--- main-9.js
+++ main-9.js
@@ -1,1 +1,43 @@
 //JS_9
+
+//functions
+
+// function dosomething() {
+//   console.log("Doing something...");
+// }
+
+function sumNumbers() {
+  console.log("hello world");
+}
+
+sumNumbers();
+sumNumbers();
+
+if (true) {
+  sumNumbers();
+}
+
+function bank() {
+  console.log("take your money");
+}
+
+let balance = 9;
+
+if (balance > 0) {
+  bank();
+} else {
+  console.log("you cant take your money");
+}
+
+//
+
+function sumNumbers2(a, b) {
+  console.log(a + b);
+}
+
+let num1 = 50;
+let num2 = 80;
+
+sumNumbers2(num1, num2);
+sumNumbers2(1234, 489);
+sumNumbers2(57, 9);
